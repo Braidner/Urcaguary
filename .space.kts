d43f94f8bc9e6657a1dc77c249c2a65081d2792e
Nick Smith <me@braidner.org>
2021-12-20 00:45:03 +0000

    First automation job

---
+++ .space.kts
@@ -0,0 +1,13 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Hello World!") {
+    container(displayName = "Say Hello", image = "hello-world")
+}
+
+job("Build and run tests") {
+   gradlew("openjdk:11", "build")
+}
